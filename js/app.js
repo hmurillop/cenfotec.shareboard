a12e2b1922e3c38cde4ce3e1898c6c41f70e6b3c
davidfr19 <davidfr19@gmail.com>
2014-08-11 20:20:25 -0600

    Angular y app.js
    Se agrego angular y el scrip del app a los html

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 (function(){
 
 	var app = angular.module('sharedboard', []);
+
+
+    app.controller('foroMakerCtrl', ['$scope', function($scope) {
+
+    	$scope.master = {};
+
+	      $scope.update = function(foro) {
+	        $scope.master = $scope.template;
+	        $scope.master[0].name = foro;
+	        console.log($scope.master[0]);
+	        $scope.foros.push($scope.master[0]);
+	        $scope.flag3 = true;
+	        $scope.master = {};
+	        console.log($scope.foros);
+	      };
+
+	         
+
+    }]);
     
 
     
