6bf60cb109a298962607d2753067a6ea8a2ae910
hmurillop <hector@Hectors-MacBook-Air.local>
2014-08-13 19:28:52 -0600

    Doc.a.js

--- js/app.js
+++ js/app.js
@@ -4,18 +4,16 @@
 
 
     app.controller('foroMakerCtrl', ['$scope', function($scope) {
-
     	$scope.master = {};
-
-	      $scope.update = function(foro) {
-	        $scope.master = $scope.template;
-	        $scope.master[0].name = foro;
-	        console.log($scope.master[0]);
-	        $scope.foros.push($scope.master[0]);
-	        $scope.flag3 = true;
-	        $scope.master = {};
-	        console.log($scope.foros);
-	      };
+    	$scope.update = function(foro) {
+    	$scope.master = $scope.template;
+    	$scope.master[0].name = foro;
+    	console.log($scope.master[0]);
+    	$scope.foros.push($scope.master[0]);
+    	$scope.flag3 = true;
+    	$scope.master = {};
+    	console.log($scope.foros);
+    };
 
 	         
 
--- php/ln/blog_ln.php
+++ php/ln/blog_ln.php
@@ -2,16 +2,13 @@
 <?php
 // define variables and set to empty values
 
-
-
-function ObtenerEntradaPorId($idBlog){
-
-	include('../cad/blog_ad.php');//Incluye el archivo de conexion a base de datos
-	$blog = new blog_ad();			
-	$datosPublicacion=$blog->ObtenerPublicacionBlogPorId($idBlog);
-	$comentariosPublicacion=$blog->ObtenerComentariosPublicacionBlog($idBlog);
+function ObtenerEntradaPorId($idBlog) {
+	include ('../cad/blog_ad.php');//Incluye el archivo de conexion a base de datos
+	$blog                   = new blog_ad();
+	$datosPublicacion       = $blog->ObtenerPublicacionBlogPorId($idBlog);
+	$comentariosPublicacion = $blog->ObtenerComentariosPublicacionBlog($idBlog);
 	//$datosPublicacion[count($datosPublicacion)]={"comentarios"=$comentariosPublicacion	};
-	$datosPublicacion['comentarios']= $comentariosPublicacion	;
+	$datosPublicacion['comentarios'] = $comentariosPublicacion;
 	echo json_encode($datosPublicacion);
 }
 
