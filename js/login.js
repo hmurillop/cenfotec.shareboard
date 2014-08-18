6734445c11d00f3fad8b9fa0c27706a3150284b2
adriancr93 <adriancr93@hotmail.com>
2014-08-18 14:05:55 -0600

    Login
    todavia la conexion no funciona hay que arreglarlo estoy en eso para que
    funcione

--- js/login.js
+++ js/login.js
@@ -1,31 +1,15 @@
-(function(){
-    var app = angular.module('login', []);
-    var pagina = 'blog_1.html';
+function formLogin($scope, $http){
+$scope.url = './php/validacionLogin.php';
+
+
+$scope.loginPage = function(login){
+
  
+  $http.post($scope.url, {"data": $scope.login}).
+   success(function(data, status){
+     alert($scope.data = data);
 
-    app.controller('LoginController', ['$scope', '$http', function($scope, $http){
-           $scope.url = './php/validacionLogin.php';     	
-
-       $http.post($scope.url,{"data": $scope.login}).success(function(data,status){
-
-       	$scope.users = data;
-
-       });
-
-       $scope.loginPage= function(login){
-       	for(var i = 0; i < $scope.users.length; i++){
-
-       		if($scope.users == login.email){
-            alert('entre');
-       			redireccionar();
-       		};
-       	};
-       };
-
-       function redireccionar(){
-       	location.href = pagina;
-       };
-    }])
-
-
-})();
+  
+   });
+  };
+}
--- php/validacionLogin.php
+++ php/validacionLogin.php
@@ -1,19 +1,24 @@
 <?php
-    $conexion = mysql_connect('127.0.0.1','root','')
-     or die("no se puede seleccionar la base de datos");
+	$conexion = mysql_connect('127.0.0.1','root','')
+	 or die("no se puede seleccionar la base de datos");
 
-    mysql_select_db("bdshareboard")
+	mysql_select_db("bdshareboard")
 	or die("No conecto esa base de datos");
+
+	$data = file_get_contents("php://input");
+    
+	$objData = json_encode($data);
 	
-	$consulta = mysql_query("SELECT * FROM tbusuario", $conexion);
-	$nfilas = mysql_num_rows($consulta);
+
+	$consulta  = mysql_query('SELECT * FROM  tbusuario WHERE email ="'. $objData->email .'" && contrasenna = "'.$objData->contrasenna.'";', $conexion);
+    $validacion = is_string($consulta);
+    print $validacion;
+
+    if($validacion === true){
+    echo header ("Location: ../blog_1.html");
+    }
+    else{
+     	print 'Error';
+     }
 	
-	if($nfilas>0){
-      for($i=0; $i<$nfilas ; $i++){
-	    $fila = mysql_fetch_array($consulta);
-	    $datos=$fila['email'];
-		echo json_encode($datos);
-	  }	
-	}
-  	
 ?>
