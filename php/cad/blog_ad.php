59d4cd92186031bb3ed02953d085426d71056f6c
davidfr19 <davidfr19@gmail.com>
2014-08-19 10:54:18 -0600

    Consultas Agregar foro

--- php/cad/blog_ad.php
+++ php/cad/blog_ad.php
@@ -69,5 +69,59 @@ class blog_ad{
 			throw $ERROR;		
 		}	
 	}
+
+	public function obtenerTodasLasCarreras($id){		
+		try{			
+				if(!Obj_BD){				
+					include('BaseDatos_AD.php');//Incluye el archivo de conexion a base de datos			
+					$this->Obj_BD = new BaseDatos_AD();						
+				}
+				$Str_Query = 'SELECT nombreCarrera FROM `tbcarrera`;';		
+				$conexion = $this->Obj_BD->Conectar();		
+				$resultado = $this->Obj_BD->SQLQuery($conexion,$Str_Query);				
+				if(!$resultado){				
+				$Obj_respuesta = '<b>ERROR:</b> No se logro realizar la transaccion.<br><b>Detalle: </b>'.mysql_error();			
+				}else{									
+						$Int_Contador = 0;					
+						while($Obj_fila = mysql_fetch_assoc($resultado)){					
+							$Obj_respuesta[$Int_Contador] = $Obj_fila;					
+							$Int_Contador++;				
+						}
+				}			
+				$this->Obj_BD->FreeMem($resultado);			
+				$this->Obj_BD->Cerrar($conexion);						
+			return $Obj_respuesta;
+			
+		}catch (Exception $ERROR){			
+			throw $ERROR;		
+		}	
+	}
+
+	public function obtenerCursosPorCarrera($id){		
+		try{			
+				if(!Obj_BD){				
+					include('BaseDatos_AD.php');//Incluye el archivo de conexion a base de datos			
+					$this->Obj_BD = new BaseDatos_AD();						
+				}
+				$Str_Query = 'SELECT `nombreCurso` FROM `tbcurso` WHERE `IdCarrera` = 10;';		
+				$conexion = $this->Obj_BD->Conectar();		
+				$resultado = $this->Obj_BD->SQLQuery($conexion,$Str_Query);				
+				if(!$resultado){				
+				$Obj_respuesta = '<b>ERROR:</b> No se logro realizar la transaccion.<br><b>Detalle: </b>'.mysql_error();			
+				}else{									
+						$Int_Contador = 0;					
+						while($Obj_fila = mysql_fetch_assoc($resultado)){					
+							$Obj_respuesta[$Int_Contador] = $Obj_fila;					
+							$Int_Contador++;				
+						}
+				}			
+				$this->Obj_BD->FreeMem($resultado);			
+				$this->Obj_BD->Cerrar($conexion);						
+			return $Obj_respuesta;
+			
+		}catch (Exception $ERROR){			
+			throw $ERROR;		
+		}	
+	}
 }
 ?>
