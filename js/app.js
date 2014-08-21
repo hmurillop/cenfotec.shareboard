615c6f89b1ac66b3f2622c17eaf7c8e551a59c15
davidfr19 <davidfr19@gmail.com>
2014-08-20 21:41:48 -0600

    Crear foro avance

--- js/app.js
+++ js/app.js
@@ -2,46 +2,4 @@
 
 	var app = angular.module('sharedboard', ['store']);
 
-    app.controller('foroMakerCtrl', ['$scope', function($scope) {
-        	$scope.master = {};
-        	$scope.update = function(foro) {
-        	$scope.master = $scope.template;
-        	$scope.master[0].name = foro;
-        	console.log($scope.master[0]);
-        	$scope.foros.push($scope.master[0]);
-        	$scope.flag3 = true;
-        	$scope.master = {};
-        	console.log($scope.foros);
-        };
-
-    }]);
-
-    
-
-    app.controller('generalForoCtrl', ['$scope', function($scope) {
-
-    	$scope.flag= false;
-    	$scope.flag1= false;
-
-    	$scope.showFormAddPost = function(){
-    		$scope.flag= true;
-    	}
-
-    	$scope.showFormAddForo = function(){
-    		$scope.flag1= true;
-    	}
-
-   
-	         
-
-    }]);
-
-
-
-    
-
-    
-
-
-
 })();
